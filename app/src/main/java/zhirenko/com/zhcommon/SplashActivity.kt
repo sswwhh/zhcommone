92844e71f959a6c629a92d811b4a2b693f2613b2
Svyatoslav Zhirenko <szhirenko@gmail.com>
2018-09-17 19:55:56 +0300

    init

---
+++ app/src/main/java/zhirenko/com/zhcommon/SplashActivity.kt
@@ -0,0 +1,13 @@
+package zhirenko.com.zhcommon
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+
+}
